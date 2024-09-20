62865c7fbc75a0104f1f15ad5301d614c19d8464
pannawat05 <pannawatlertkomenkul@gmail.com>
2024-09-20 10:09:01 +0700

    make it better

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ app.put("/update", (req, res) => {
   });
   
 
-app.listen('3001',()=>{
-    console.log('Server is running on port 3001 ');
-})
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
